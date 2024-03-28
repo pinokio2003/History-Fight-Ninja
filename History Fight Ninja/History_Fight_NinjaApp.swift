09290fb10425675984f0ba6042c0e1028347ff5c
pinokio2003 <pinokio0476@gmail.com>
2024-03-28 17:02:50 +0200

    Initial Commit

---
+++ History Fight Ninja/History_Fight_NinjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  History_Fight_NinjaApp.swift
+//  History Fight Ninja
+//
+//  Created by Anatolii Kravchuk on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct History_Fight_NinjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
